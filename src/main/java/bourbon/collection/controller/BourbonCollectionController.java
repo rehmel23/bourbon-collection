bb0604035bdf2f094603b708a5817fe5a96630a3
Clay Rehmel <clay.rehmel@gmail.com>
2023-08-23 07:33:12 -0400

    Code update as of 8/23. POST for bottle and distillery both post to respective tables. Return in ARC for bottle is an infinite loop.

--- src/main/java/bourbon/collection/controller/BourbonCollectionController.java
+++ src/main/java/bourbon/collection/controller/BourbonCollectionController.java
@@ -2,12 +2,15 @@ package bourbon.collection.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import bourbon.collection.controller.model.BottleData;
+import bourbon.collection.controller.model.BottleData.BourbonDistiller;
 import bourbon.collection.service.BottleService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,14 +18,33 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/bourbon_collection")
 @Slf4j
 public class BourbonCollectionController {
-	
+
 	@Autowired
 	private BottleService bottleService;
 
-	@PostMapping("/bottle")
+	// Infinite Loop on return, PUTs into DB correctly.
+	@PostMapping("/{distillerId}/bottle")
 	@ResponseStatus(code = HttpStatus.CREATED)
-	public BottleData insertBottle(BottleData bottleData) {
-		log.info("Adding bottle {} to collection", bottleData);
-		return bottleService.saveBottle(bottleData);
+	public BottleData insertBottle(@PathVariable Long distillerId, @RequestBody BottleData bottleData) {
+		log.info("Adding bottle {} to collection made by distiller {}.", bottleData, distillerId);
+		return bottleService.saveBottle(bottleData, distillerId);
 	}
+
+	/**
+	 * PUT new distillery in database
+	 * 
+	 * @param bourbonDistiller
+	 * @return Distiller information
+	 */
+	@PostMapping("/distiller")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public BourbonDistiller addDistiller(@RequestBody BourbonDistiller bourbonDistiller) {
+		log.info("Adding distiller {}.", bourbonDistiller.getDistillerName());
+		return bottleService.saveDistiller(bourbonDistiller);
+	}
+
+	/**
+	 * GET all distilleries to make bottle POST easier for user to find distillerId.
+	 */
+
 }
--- src/main/java/bourbon/collection/service/BottleService.java
+++ src/main/java/bourbon/collection/service/BottleService.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import bourbon.collection.controller.model.BottleData;
+import bourbon.collection.controller.model.BottleData.BourbonDistiller;
 import bourbon.collection.dao.BottleDao;
+import bourbon.collection.dao.DistillerDao;
 import bourbon.collection.entity.Bottle;
+import bourbon.collection.entity.Distiller;
 
 @Service
 public class BottleService {
@@ -17,15 +20,19 @@ public class BottleService {
 	@Autowired
 	private BottleDao bottleDao;
 
+	@Autowired
+	private DistillerDao distillerDao;
+
 	@Transactional(readOnly = false)
-	public BottleData saveBottle(BottleData bottleData) {
-		Long bottleId = bottleData.getBottleId();
-		Bottle bottle = findOrCreateBottle(bottleId);
-
+	public BottleData saveBottle(BottleData bottleData, Long distillerId) {
+		Distiller distiller = findDistillerById(distillerId);
+		Bottle bottle = findOrCreateBottle(distillerId, bottleData.getBottleId());
+		
 		copyBottleFields(bottle, bottleData);
-
-		Bottle dbBottle = bottleDao.save(bottle);
-		return new BottleData(dbBottle);
+		bottle.setDistiller(distiller);
+		distiller.getBottles().add(bottle);
+		
+		return new BottleData(bottleDao.save(bottle));
 	}
 
 	private void copyBottleFields(Bottle bottle, BottleData bottleData) {
@@ -37,21 +44,63 @@ public class BottleService {
 		bottle.setDistiller(bottleData.getDistiller());
 	}
 
-	private Bottle findOrCreateBottle(Long bottleId) {
+	private Bottle findOrCreateBottle(Long distillerId, Long bottleId) {
 		Bottle bottle;
 
 		if (Objects.isNull(bottleId)) {
 			bottle = new Bottle();
 		} else {
-			bottle = findBottleById(bottleId);
+			bottle = findBottleById(distillerId, bottleId);
 		}
 
 		return bottle;
 	}
 
-	private Bottle findBottleById(Long bottleId) {
-		return bottleDao.findById(bottleId)
+	private Bottle findBottleById(Long distillerId, Long bottleId) {
+		Bottle bottle = bottleDao.findById(bottleId)
 				.orElseThrow(() -> new NoSuchElementException("Bottle with ID=" + bottleId + " does not exist."));
+
+		if (bottle.getDistiller().getDistillerId() != distillerId) {
+			throw new IllegalArgumentException(
+					"Bottle with ID=" + bottleId + " is not made by distiller with ID=" + distillerId);
+		}
+
+		return bottle;
+	}
+
+	public BourbonDistiller saveDistiller(BourbonDistiller bourbonDistiller) {
+		Long distillerId = bourbonDistiller.getDistillerId();
+		Distiller distiller = findOrCreateDistiller(distillerId);
+
+		copyDistillerFields(distiller, bourbonDistiller);
+
+		Distiller dbDistiller = distillerDao.save(distiller);
+		return new BourbonDistiller(dbDistiller);
+	}
+
+	private void copyDistillerFields(Distiller distiller, BourbonDistiller bourbonDistiller) {
+		distiller.setDistillerId(bourbonDistiller.getDistillerId());
+		distiller.setDistillerName(bourbonDistiller.getDistillerName());
+		distiller.setCity(bourbonDistiller.getCity());
+		distiller.setState(bourbonDistiller.getState());
+		distiller.setZip(bourbonDistiller.getZip());
+	}
+
+	private Distiller findOrCreateDistiller(Long distillerId) {
+		Distiller distiller;
+
+		if (Objects.isNull(distillerId)) {
+			distiller = new Distiller();
+		} else {
+			distiller = findDistillerById(distillerId);
+		}
+
+		return distiller;
+	}
+
+	private Distiller findDistillerById(Long distillerId) {
+		return distillerDao.findById(distillerId)
+				.orElseThrow(() -> new NoSuchElementException("Distiller with ID=" + distillerId + " does not exist."));
 	}
 
 }
